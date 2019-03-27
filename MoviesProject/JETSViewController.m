8df356ed1212e9ab36494593ef6bb73b40cffd0f
JETS <participant@Ahmeds-Mac.local>
2019-03-27 09:28:10 +0200

    Initial Commit

---
+++ MoviesProject/JETSViewController.m
@@ -0,0 +1,29 @@
+//
+//  JETSViewController.m
+//  MoviesProject
+//
+//  Created by JETS on 3/27/19.
+//  Copyright (c) 2019 JETS. All rights reserved.
+//
+
+#import "JETSViewController.h"
+
+@interface JETSViewController ()
+
+@end
+
+@implementation JETSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
